c3dff456c87cea3da692535dd68f970db92efee7
Julien Ait Azzouzene <julien.azzo27@gmail.com>
2025-04-15 18:45:43 +0200

    feat: connexion du bot à de Discord
    N.B. il semblerait que les dépendances ne permettent pas de ciblier en
    dessous de ES2020. J'ai donc mis à jour la cible.
    
    C'est pas très important mais je le souligne quand même.
    
    Refs: #1

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log('Hello, world!');
+import { Client, Events, GatewayIntentBits } from "discord.js";
+
+const discordClient = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+discordClient.once(Events.ClientReady, onReadyClient);
+
+discordClient.login(process.env.DISCORD_TOKEN).catch((error) => {
+    console.error("Failed to login to Discord: ", error);
+});
+
+function onReadyClient(client: Client): void {
+    console.log(`Ready! Logged in as ${client.user?.tag}`);
+}
