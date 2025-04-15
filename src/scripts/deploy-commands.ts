d93a43eeeb227bd80d9d1c9fcc53f97f3c69c056
Julien Ait Azzouzene <julien.azzo27@gmail.com>
2025-04-15 20:29:34 +0200

    chore: renommage d'une variable + message d'erreur
    DISCORD_APPLICATION_ID correspond au champ "Application ID" visible sur
    l'espace développeur de Discord. Il est aussi parfois appelé
    "client_id".
    
    J'ai corrigé le message d'erreur quand cette variable n'est pas définie,
    et j'ai renommé la variable affectée dans le code pour plus de clarté.
    
    Refs: #1

--- src/scripts/deploy-commands.ts
+++ src/scripts/deploy-commands.ts
@@ -3,14 +3,14 @@ import commandList from "../commands/commands";
 
 async function deployCommands() {
     const token = process.env.DISCORD_TOKEN;
-    const clientId = process.env.DISCORD_APPLICATION_ID;
+    const applicationClientId = process.env.DISCORD_APPLICATION_ID;
     const guildId = process.env.DISCORD_SERVER_ID;
 
     if(! token) {
         throw new Error("DISCORD_TOKEN environment variable is not defined");
     }
-    if(! clientId) {
-        throw new Error("DISCORD_CLIENT_ID environment variable is not defined");
+    if(! applicationClientId) {
+        throw new Error("DISCORD_APPLICATION_ID environment variable is not defined");
     }
     if(! guildId) {
         throw new Error("DISCORD_SERVER_ID environment variable is not defined");
@@ -24,7 +24,7 @@ async function deployCommands() {
         console.log(`Started refreshing ${commands.length} application (/) commands.`);
 
         const data = await rest.put(
-            Routes.applicationGuildCommands(clientId, guildId),
+            Routes.applicationGuildCommands(applicationClientId, guildId),
             { body: commands },
         );
 
