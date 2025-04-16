07c8958b75551f8e5ec2587476cdcef7402538f9
Julien Ait Azzouzene <julien.azzo27@gmail.com>
2025-04-16 20:40:52 +0200

    chore: ESLint ignore les dossiers node_modules et dist
    Refs: #3

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,13 @@
 import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
-import { defineConfig } from "eslint/config";
+import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
+  globalIgnores([
+    "node_modules/",
+    "dist/",
+  ]),
   {
     files: ["**/*.{js,mjs,cjs,ts}"],
     plugins: { js },
