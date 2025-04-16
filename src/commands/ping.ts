5c9e73ea61657b46c68cb3a2fa58f07573d7f611
Julien Ait Azzouzene <julien.azzo27@gmail.com>
2025-04-16 20:03:30 +0200

    fix: typage des intéractions pour les handler de commandes
    Refs: #3

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -1,14 +1,14 @@
-import { SlashCommandBuilder } from "discord.js";
+import { ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js";
 import { Command } from "../types/command";
 
 const data = new SlashCommandBuilder()
-    .setName('ping')
-    .setDescription('Replies with Pong!')
+  .setName('ping')
+  .setDescription('Replies with Pong!')
 ;
 
-async function execute(interaction: any): Promise<void> {
-    //TODO strongly type interaction
-    await interaction.reply('Pong!');
+async function execute(interaction: ChatInputCommandInteraction): Promise<void> {
+  //TODO strongly type interaction
+  await interaction.reply('Pong!');
 }
 
 const command: Command = { data, execute };
